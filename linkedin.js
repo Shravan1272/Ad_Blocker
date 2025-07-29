2e24e81eedc42ef129a1b353bf8fc4ff3615ce01
Shravan Kanherkar <shravankanherkar1272@gmail.com>
2025-07-29 15:35:23 +0530

    Updated linked.js to support new LinkedIn ad DOM structure

---
+++ linkedin.js
@@ -0,0 +1,34 @@
+function removeads(){
+    //to get all span elemenrts on the page
+    let elements = [...document.getElementsByTagName("p"), ...document.getElementsByTagName("span")];
+
+    for(let i=0; i<elements.length;i++){
+        //to check if they the text "promoted"
+        if(elements[i]==innerHTML.includes("Promoted")){
+            let card=elements[i].closest(".feed-shared-update-v2");
+
+            //if the class changed and we can't find it with closest(), get 6th parent
+            if(card==null){
+                let temp=elements[i];
+                card = spans[i];
+                for(let j=0;j<6;++j){
+                    if(!temp.parentNode)
+                        break;
+                    temp=temp.parentNode;
+                }
+                card=temp;
+            }
+            //making the add disappear!!
+            if(card){
+                card.setAttribute("style","display: none !important;");
+            }
+        }
+    }
+}
+
+//to remove ads as the user scrolls down
+// Run initially
+removeAds();
+
+// Keep checking as user scrolls
+setInterval(removeads, 100);
